fd20a47fd75b0ccab0fbddd0099429438a71c04c
Irene Havsa <irene.ully@gmail.com>
2025-09-13 17:05:00 +0700

    Challenge 2 completed

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogSpawnCooldown = 0.6f;
+    private bool dogSpawnEnabled = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogSpawnEnabled)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            dogSpawnEnabled = false;
+            Invoke("EnableDogSpawn", dogSpawnCooldown);
+        }
+    }
+
+    void EnableDogSpawn()
+    {
+        dogSpawnEnabled = true;
+    }
+}
