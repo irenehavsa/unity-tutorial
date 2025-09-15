1529ebf70bb50824d813821a26fdb512f923d632
Irene Havsa <irene.ully@gmail.com>
2025-09-15 15:05:47 +0700

    Update Prototype 1 to use SerializeField and FixedUpdate

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -3,13 +3,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 6, -9);
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Vector3 offset = new Vector3(0, 6, -9);
 
     // Update is called once per frame
     void LateUpdate()
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Move the vehicle forward based on vertical input
         forwardInput = Input.GetAxis("Vertical");
